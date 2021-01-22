67851a06e69d492676d06b64eed6acd2b3636b9a
Nicolas-Li <lijunjie_thu@qq.com>
2021-01-22 21:09:28 +0800

    完成701

---
+++ 701/Sol_题解.py
@@ -0,0 +1,26 @@
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return TreeNode(val)
+        
+        pos = root
+        while pos:
+            if val < pos.val:
+                if not pos.left:
+                    pos.left = TreeNode(val)
+                    break
+                else:
+                    pos = pos.left
+            else:
+                if not pos.right:
+                    pos.right = TreeNode(val)
+                    break
+                else:
+                    pos = pos.right
+        
+        return root
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/insert-into-a-binary-search-tree/solution/er-cha-sou-suo-shu-zhong-de-cha-ru-cao-zuo-by-le-3/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
